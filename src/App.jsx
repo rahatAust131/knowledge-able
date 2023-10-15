5e7cbb794883b2e200a87acd724cd65007a1d45d
Rahat Chowdhury <rczisan@gmail.com>
2023-10-15 11:14:54 +0600

    Added all the necessary samples for vite project

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <div>
+        HI there!
+      </div>
+    </>
+  )
+}
+
+export default App
